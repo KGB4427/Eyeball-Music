07a9393b066ce3be2017dfccbf6f750dd73ea5cf
KGB4427 <shadowking4427@gmail.com>
2024-10-29 10:45:37 -0500

    Added the eyes

--- script.js
+++ script.js
@@ -6,17 +6,38 @@ let beatInterval = (60 / bpm) * 1000; // Calculate time between beats in millise
 let audioOn = false;
 let maxRadSlider; // Slider for maximum radius
 
+let scene3pupils = [];
+let scene3eyeliner = [];
+let scene2pupils = [];
+let scene2eyeliner = [];
+
 
 let volSense = 200; // Volume sensitivity
 let sliderStep = 1; // Slider step size
 
 let freqThreshold = 150; // Set the threshold for bass energy 0 is high 200 is low
 
-let globeScale = 1.6; // 16:10 aspect ratio
+let ratio = 1.6; // 16:10 aspect ratio
+let globeScale;
 let maxRadius; // Maximum radius of the circle
 
 function preload() {
-    
+    // for( let i = 1; i <= 4; i++) {
+    //     scene2pupils.push(loadImage(`./photos/scene2/Sc2_Pupil${i}.png`));
+    // }
+
+    // for( let i = 1; i <= 4; i++) {
+    //     scene2eyeliner.push(loadImage(`./photos/scene2/Sc2_Eyeliner${i}.png`));
+    // }
+
+    for (let i = 1; i <= 5; i++) {
+        scene3pupils.push(loadImage(`./photos/scene3/pupil${i}.png`));
+    }
+
+    for (let i = 1; i <= 5; i++) {
+        scene3eyeliner.push(loadImage(`./photos/scene3/pupil${i}.png`));
+    }
+
 }
 
 function setup() {
@@ -27,18 +48,15 @@ function setup() {
 
     maxRadSlider = new sliders(0, 100, 50, sliderStep);
     maxRadSlider.position(10, 50);
-    maxRadSlider.text('Maxiumum Radius');
+    maxRadSlider.text('Maximum Radius');
 
-    createCanvas(window.innerWidth / globeScale, window.innerHeight / globeScale);
+    createCanvas(window.innerWidth, window.innerWidth / ratio);
+    globeScale = min(width, height);
     getAudioContext().suspend();
     fft = new p5.FFT();
     mic = new p5.AudioIn();
     mic.start();
     fft.setInput(mic);
-
-    maxRadius = min(width, height) / (2*globeScale); // Set the maximum radius of the circle
-
-    
     
 }
   
@@ -59,6 +77,8 @@ function draw() {
         }
         
     }
+
+    
     scene1();
 
 }
@@ -77,6 +97,9 @@ function scene1() {
     let radiusC = map(timeSinceLastBeat, 0, beatInterval, 0, maxRadius);
     radiusC = min(radiusC, maxRadius);
     
+    maxRadius = globeScale * maxRadSlider.slider.value() / 100; // Set the maximum radius of the circle
+    //console.log("Max Radius: ", maxRadius); // Log the maximum radius for debugging
+
     ellipse(width/3, height / 3, radiusC, radiusC); // Move the circle with the beat
     ellipse(width/1.5, height / 3, radiusC, radiusC); // Move the circle with the beat
 
@@ -93,15 +116,22 @@ function scene2() {
     
     ellipse(width/3, height / 3, radiusC, radiusC); // Move the circle with the beat
     ellipse(width/1.5, height / 3, radiusC, radiusC); // Move the circle with the beat
+    for( let i = 1; i <= 4; i++) {
+        image(scene2eyeliner[i], 0, 0);
+        image(scene2pupils[i], 0, 0);
+    }
     
 }
 
 // 10-15 pairs of eyes
 function scene3() {
     
+    image(scene3pupils[0], 300, 300, 100, 100);
+    
 }
 
-function mousePressed() {
-    console.log("width / " + width/mouseX);
-    console.log("height / " + height/mouseY);
-}
+// function mousePressed() {
+//     console.log("width / " + width/mouseX);
+//     console.log("height / " + height/mouseY);
+//     AudioContext.resume();
+// }
