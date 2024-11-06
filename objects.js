f5b1a90b2078f2dedcdd1d4f01b5cef293e368ca
KGB4427 <shadowking4427@gmail.com>
2024-11-06 09:45:53 -0600

    Cutting Scene 3
    Until we can figure out how to implement scene 3 in a cleaner way with a better composition and animation, it's being cut.

--- objects.js
+++ objects.js
@@ -12,23 +12,23 @@ class sliders {
 
     text(label) {
         this.label = label; // Set the label of the slider
-        //this.labelP.style('color', 'white'); // Set the color of the label text
+        this.labelP.style('color', 'white'); // Set the color of the label text
         this.labelP.html(label); // Update the paragraph element with the label text
     }
 }
 
 
-class Eyes {
+class Sc3EYES {
     constructor(i) {
         this.i = i;
         // Calculate the positions and sizes based on the canvas dimensions and scaling factors
         this.imgWidth = globeScale * 0.3; // Adjust the divisor to control the size
-        this.imgHeight = random(globeScale*0.1, globeScale*0.5); // Adjust the divisor to control the size
+        this.imgHeight = globeScale * 0.5; // Adjust the divisor to control the size
 
 
         // Calculate positions based on the canvas dimensions and scaling factors
-        this.xPos = width / 6; // Adjust the divisor to control the position
-        this.yPos = height / 2; // Adjust the divisor to control the position
+        this.xPos = width * 0.0001; // Adjust the divisor to control the position
+        this.yPos = height * 0.25; // Adjust the divisor to control the position
 
         // Constrain the pupil to the position of the eyeliner image
         this.initialPupilX = this.xPos + i * this.imgWidth / 1.2;
@@ -42,32 +42,13 @@ class Eyes {
         // Check if there is active sound (bassEnergy above a threshold)
         this.bassThreshold = 160; // Adjust this threshold as needed
         this.trebleThreshold = 0; // Adjust this threshold as needed
-        this.midThreshold = 0; // Adjust this threshold as needed
+        this.midThreshold = 90; // Adjust this threshold as needed
 
-        this.shakingIntensity = volSenseSlider.slider.value(); // Adjust this value as needed
     }
 
     displayEyes() {
         if (bassEnergy > this.bassThreshold || midEnergy > this.midThreshold || trebleEnergy > this.trebleThreshold) {
-            let xoff = 0;
-            let yoff = 1000;
-            // Use noise to generate smooth random values for the pupil positions
-            let baseNoiseOffsetX = noise(xoff * lerp(0, highMidEnergy, 0.001)) * this.imgWidth / 4 - this.imgWidth / 8; // Slower noise
-            let baseNoiseOffsetY = noise(yoff * lerp(0, bassEnergy, 0.001)) * this.imgHeight / 4 - this.imgHeight / 8; // Slower noise
-
-            xoff += 10; 
-            yoff += 10;
-
-            // Scale the noise offsets by the volSenseSlider value to control shaking intensity
-            // Combine base movement and bass energy movement
-            //this.pupilX = this.initialPupilX + (baseNoiseOffsetX * highMidEnergy) * this.shakingIntensity / 1000;
-            this.pupilY = this.initialPupilY + (baseNoiseOffsetY * bassEnergy) * this.shakingIntensity / 5000;
-            //this.pupilX += (baseNoiseOffsetX * highMidEnergy) * shakingIntensity / 1000;
-            //this.pupilY += (baseNoiseOffsetY * bassEnergy) * this.shakingIntensity / 5000;
-
-            // Constrain the pupil positions to the canvas
-            this.pupilX = constrain(this.pupilX, 0, width - this.pupilSize / 2); // Adjusted to move the eyes up
-            this.pupilY = constrain(this.pupilY, 0, height - this.pupilSize / 2); // Adjusted to move the eyes up
+            
         }
 
         imageMode(CENTER);
@@ -85,11 +66,11 @@ class Sc2EYES {
 
         this.i = i;
 
-        this.R = map(this.energyR, 0, 400, 0, 255);
-        this.G = map(this.energyG, 0, 400, 0, 255);
-        this.B = map(this.energyB, 0, 400, 0, 255);
+        this.R = map(this.energyR, 0, volSenseSlider.slider.value(), 0, 255);
+        this.G = map(this.energyG, 0, volSenseSlider.slider.value(), 0, 255);
+        this.B = map(this.energyB, 0, volSenseSlider.slider.value(), 0, 255);
 
-        tint(0, 0, 100);
+        tint(0, 0, 250);
         image(scene2eyeliner[i % scene2eyeliner.length], 0, 0, width, height);
         tint(this.R, this.G, this.B);
         image(scene2pupils[i % scene2pupils.length], 0, 0, width, height);
--- script.js
+++ script.js
@@ -21,7 +21,7 @@ let scene1eyes;
 let currentScene = 1;
 let sceneStartTime;
 
-let volSense = 200; // Volume sensitivity
+let volSense = 100; // Volume sensitivity
 let sliderStep = 1; // Slider step size
 
 let freqThreshold = 150; // Set the threshold for bass energy 0 is high 200 is low
@@ -64,13 +64,14 @@ function preload() {
 }
 
 function setup() {
-    volSenseSlider = new sliders(20, volSense * 2, volSense / 2, sliderStep);
+    volSenseSlider = new sliders(1, volSense, volSense / 2, sliderStep);
     volSenseSlider.position(10, 10);
     volSenseSlider.text('Volume Sensitivity');
+    volSenseSlider.slider.id('sensitivitySlider'); // Assign an ID to the slider
 
-    maxRadSlider = new sliders(10, 30, 15, sliderStep);
-    maxRadSlider.position(10, 50);
-    maxRadSlider.text('Maximum Radius');
+    // maxRadSlider = new sliders(10, 30, 15, sliderStep);
+    // maxRadSlider.position(10, 50);
+    // maxRadSlider.text('Maximum Radius');
 
     createCanvas(window.innerWidth, window.innerWidth / ratio);
     globeScale = min(width, height);
@@ -81,11 +82,6 @@ function setup() {
     fft.setInput(mic);
 
     sceneStartTime = millis(); // Initialize the scene start time
-
-    // Create instances of the Eyes class and store them in the eyes array
-    for (let i = 0; i < scene3eyeliner.length; i++) {
-        eyesObject.push(new Eyes(i));
-    }
     
     colorMode(HSB, 100);
 }
@@ -103,11 +99,11 @@ function draw() {
         highMidEnergy = fft.getEnergy("highMid"); // High mid frequency energy
         trebleEnergy = fft.getEnergy("treble"); // High frequency energy
 
-        console.log("Bass Energy: ", bassEnergy); // Log bass energy for debugging
-        console.log("Low Mid Energy: ", lowMidEnergy); // Log low mid energy for debugging
-        console.log("Mid Energy: ", midEnergy); // Log mid energy for debugging
-        console.log("High Mid Energy: ", highMidEnergy); // Log high mid energy for debugging
-        console.log("Treble Energy: ", trebleEnergy); // Log treble energy for debugging
+        // console.log("Bass Energy: ", bassEnergy); // Log bass energy for debugging
+        // console.log("Low Mid Energy: ", lowMidEnergy); // Log low mid energy for debugging
+        // console.log("Mid Energy: ", midEnergy); // Log mid energy for debugging
+        // console.log("High Mid Energy: ", highMidEnergy); // Log high mid energy for debugging
+        // console.log("Treble Energy: ", trebleEnergy); // Log treble energy for debugging
 
         // Check for a beat (if bass energy exceeds a threshold)
         if (bassEnergy > freqThreshold && millis() - lastBeatTime > beatInterval * 0.8) {
@@ -120,8 +116,8 @@ function draw() {
     let elapsedTime = millis() - sceneStartTime;
     //console.log(`Elapsed Time: ${elapsedTime}, Current Scene: ${currentScene}`);
 
-    // Switch scenes every 10 seconds (10000 milliseconds)
-    if (elapsedTime > 10000) {
+    // Switch scenes every 30 seconds (30000 milliseconds)
+    if (elapsedTime > 30000) {
         currentScene++;
         if (currentScene > 3) {
             currentScene = 1; // Loop back to scene1
@@ -138,9 +134,9 @@ function draw() {
     } else if (currentScene === 2) {
         sceneTransition();
         scene2();
-    } else if (currentScene === 3) {
-        sceneTransition();
-        scene3();
+    // } else if (currentScene === 3) {
+    //     sceneTransition();
+    //     scene2();
     }
 }
 
@@ -156,12 +152,12 @@ function mousePressed() {
 function scene1() {
     let angle = 0;
 
-    fill(map(midEnergy, 0, 400, 0, 255), 100, 50);
+    fill(map(trebleEnergy, 0, volSenseSlider.slider.value(), 0, 100), 100, 50); // Hue Shift
     let timeSinceLastBeat = millis() - lastBeatTime;
     let radiusC = map(timeSinceLastBeat, 0, beatInterval, 0, maxRadius);
-    maxRadius = globeScale * maxRadSlider.slider.value() / 10000; // Set the maximum radius of the circle
+    maxRadius = globeScale / 500; // Set the maximum radius of the circle
 
-    radiusC = bassEnergy * maxRadius; // Set the radius of the circle based on the bass energy
+    radiusC = lowMidEnergy * maxRadius; // Set the radius of the circle based on the bass energy
 
     //console.log("Max Radius: ", maxRadius); // Log the maximum radius for debugging
 
@@ -182,18 +178,20 @@ function scene2() {
 
     //ellipse(width / 3, height / 3, radiusC, radiusC); // Move the circle with the beat
     //ellipse(width / 1.5, height / 3, radiusC, radiusC); // Move the circle with the beat
-    new Sc2EYES(trebleEnergy, 100, 100, 1);
-    new Sc2EYES(lowMidEnergy, 100, 100, 2);
-    new Sc2EYES(midEnergy, 100, 100, 3);
-    new Sc2EYES(highMidEnergy, 100, 100, 4);
+    let Sc2Brightness = map(highMidEnergy, 0, volSenseSlider.slider.value(), 0, 100);
+    let Sc2Saturation = map(midEnergy, 0, volSenseSlider.slider.value(), 0, 100);
+
+
+    new Sc2EYES(trebleEnergy, Sc2Saturation, Sc2Brightness, 1);
+    new Sc2EYES(trebleEnergy, Sc2Saturation, Sc2Brightness, 2);
+    new Sc2EYES(highMidEnergy, Sc2Saturation, Sc2Brightness, 3);
+    new Sc2EYES(highMidEnergy, Sc2Saturation, Sc2Brightness, 4);
 }
 
 // 10-15 pairs of eyes
 function scene3() {
     // Iterate over the eyes array and call displayEyes for each instance
-    for (let i = 0; i < eyesObject.length; i++) {
-        eyesObject[i].displayEyes();
-    }
+    
 }
 
 function sceneTransition() {
